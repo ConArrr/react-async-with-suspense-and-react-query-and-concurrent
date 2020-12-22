1db093f14ffbc554c1733e2699c144d348efa3c4
ConArrr <conarrr7714@gmail.com>
2020-12-22 13:36:05 +0900

    Dev -> Create Main Components.

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import React, { FC } from 'react'
 import './App.css'
 
 const App: FC = () => (
-  <div className="App">
-    <header className="App-header" />
-  </div>
+  <>
+    <div />
+  </>
 )
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,33 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
+import { unstable_createRoot as createRoot } from 'react-dom'
+import { ReactQueryConfig, ReactQueryConfigProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query-devtools'
+
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
-ReactDOM.render(
-  <React.StrictMode>
+import 'semantic-ui-css/semantic.min.css'
+import './index.css'
+
+const queryConfig: ReactQueryConfig = {
+  shared: {
+    suspense: true,
+  },
+  queries: {
+    retry: 0,
+    staleTime: 60 * 1000,
+  },
+}
+
+const root = document.getElementById('root') as HTMLElement
+
+createRoot(root).render(
+  <ReactQueryConfigProvider config={queryConfig}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
+    {process.env.NODE_ENV === 'development' && (
+      <ReactQueryDevtools initialIsOpen={false} />
+    )}
+  </ReactQueryConfigProvider>,
 )
 
 // If you want to start measuring performance in your app, pass a function
